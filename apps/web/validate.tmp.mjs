da34c5ca36cd5217f4729dcd7fb04327ec937d38
Vinicius Leal <viniciusleal.s@gmail.com>
2026-08-22 23:15:33 -0300

    chore: remove script temporário de validação + ignore pattern

--- apps/web/validate.tmp.mjs
+++
@@ -1,4 +0,0 @@
-import { readFileSync } from "node:fs";
-import { accessScoreResult } from "@rio-preto/domain";
-accessScoreResult.parse(JSON.parse(readFileSync("public/data/acesso/metrics.json", "utf8")));
-console.log("acesso artifact valid");
